9f8e5dd493be1540bf149fefa22230eacf024a4a
SnickErzIsBack <thiemo.neumann@web.de>
2021-06-17 10:18:57 +0200

    04_toString_function_added,_TEMPLATE_LITERALS

--- block.js
+++ block.js
@@ -1,9 +1,20 @@
 class Block{
     // Constructor + Parameter
     constructor(timestamp, lastHash, hash, data){
-        this.timestamp = timestamp;
-        this.lastHash = lastHash;
-        this.hash = hash;
-        this.data = data;
+        this.timestamp = timestamp; //Zeitstempel
+        this.lastHash = lastHash; //Hash des vorhergehenden Blocks
+        this.hash = hash; //Hash des aktuellen Blocks
+        this.data = data; //Inhalt des aktuellen Blocks
+    }
+
+    toString(){ //für De-Bugging-Zwecke, Inhalt des Obj.
+            return ` Block - 
+            Timestamp:  ${this.timestamp}
+            Last Hash:  ${this.lastHash.substring(0,10)}
+            Hash:       ${this.hash.substring(0,10)}
+            Data:       ${this.data}
+            `;
+    
+    
     }
 }
